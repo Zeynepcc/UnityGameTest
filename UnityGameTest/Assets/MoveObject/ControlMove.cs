d63896a47c51b89e2db5730d37feff5a538571fb
Zeynep Turhan <zeynepturhan0@gmail.com>
2020-12-31 03:25:10 +0300

    First Commit
    Added Unity Test Project

---
+++ UnityGameTest/Assets/MoveObject/ControlMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlMove : MonoBehaviour
+{
+
+
+    public float speed = 5f;
+    public Vector3 vec;
+    public float MoveLeft()
+    {
+        vec.x -= speed * Time.deltaTime;
+        transform.position = vec;
+        return transform.position.x;
+
+    }
+    public float MoveRight()
+    {
+        vec.x += speed * Time.deltaTime;
+        transform.position = vec;
+        return transform.position.x;
+    }
+    public float MoveUp()
+    {
+        vec.y += speed * Time.deltaTime;
+        transform.position = vec;
+        return transform.position.y;
+    }
+    public float MoveDown()
+    {
+        vec.y -= speed * Time.deltaTime;
+        transform.position = vec;
+        return transform.position.y;
+    }
+
+
+   /* //void Update()
+    {
+        vec = transform.position;
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            vec.y += speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            vec.y -= speed * Time.deltaTime;
+            
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            vec.x += speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            vec.x -= speed * Time.deltaTime;
+        }
+        transform.position = vec;
+    }*/
+}
---
+++ UnityGameTest/Assets/Tests/MoveTest.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class MoveTest
+    {
+        [UnityTest]
+        public IEnumerator MoveRight()
+        {
+            var gameObject = new GameObject();
+            var square = gameObject.AddComponent<ControlMove>();
+            double oldlocation = square.vec.x;
+            Debug.Log(oldlocation);
+
+            yield return new WaitForSeconds(5);
+
+            double moveRightLocation = square.MoveRight();
+            Debug.Log(moveRightLocation);
+
+            Assert.IsTrue(oldlocation < moveRightLocation );
+        }
+
+
+
+
+
+
+        [UnityTest]
+        public IEnumerator MoveLeft()
+        {
+            var gameObject = new GameObject();
+            var square = gameObject.AddComponent<ControlMove>();
+            double oldlocation = square.vec.x;
+
+            yield return new WaitForSeconds(5);
+
+            double moveLeftLocation = square.MoveLeft();
+
+            Assert.IsTrue(oldlocation > moveLeftLocation);
+        }
+        [UnityTest]
+        public IEnumerator MoveUp()
+        {
+            var gameObject = new GameObject();
+            var square = gameObject.AddComponent<ControlMove>();
+            float oldlocation = square.vec.y;
+
+            yield return new WaitForSeconds(5);
+
+            float moveUpLocation = square.MoveUp();
+
+            Assert.IsTrue(oldlocation < moveUpLocation);
+        }
+        [UnityTest]
+        public IEnumerator MoveDown()
+        {
+            var gameObject = new GameObject();
+            var square = gameObject.AddComponent<ControlMove>();
+            double oldlocation = square.vec.y;
+
+            yield return new WaitForSeconds(5);
+
+           double moveDownLocation = square.MoveDown();
+
+            Assert.IsTrue(oldlocation > moveDownLocation);
+        }
+    }
+}
